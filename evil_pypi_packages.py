8b918dfbf3316b8100578c6c0062ec491501c799
ridgit <ridouwanyuna@gmail.com>
2024-12-01 16:58:53 -0500

    Update evil_pypi_packages.py

--- evil_pypi_packages.py
+++ evil_pypi_packages.py
@@ -2,20 +2,29 @@ import random
 
 # Chemin du fichier requirements existant
 input_file = "evaluate_work/requirements.txt"
+"""
+ "reqeusts",
+ "urlib3",
+ "djang0",
+ "matplolib",
+ "python3-dateutil",
+ "jeilyfish",
+ "pyppyn",
+ "tornado-http",
+ "pkg-lib",
+ "aws-sdk",
+ "aiohttp-sessions",
+"""
 
 # Liste de versions vulnérables ou malveillantes de packages
 evil_packages = [
-    "reqeusts",
-    "urlib3",
-    "djang0",
-    "matplolib",
-    "python3-dateutil",
-    "jeilyfish",
-    "pyppyn",
-    "tornado-http",
-    "pkg-lib",
-    "aws-sdk",
-    "aiohttp-sessions",
+    "requests==2.19.0",  # Vulnérabilité CVE-2018-18074
+    "flask==0.12.2",  # Vulnérabilité CVE-2018-1000656
+    "django==2.0.0",  # Vulnérabilité CVE-2018-6188
+    "numpy==1.19.4",  # Vulnérabilité CVE-2021-41495
+    "pandas==1.1.0",
+    "scipy==1.5.2",
+    "matplotlib==3.3.0",
 ]
 
 # Lire les dépendances existantes
